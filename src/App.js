3729e174c42440d3520ca7a4befd70bd3c30e438
Rajat-soni-vst-au4 <Rajatsoni.rs66@gmail.com>
2022-10-05 12:42:34 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+// import './App.css';
+import Tictactoe from './Components/Tictactoe';
+
+function App() {
+  return (
+    <div className="App">
+      <Tictactoe />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Tictactoe.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function Tictactoe() {
+  return (
+    <>
+      <h1 className='text-center'>Tic tac toe</h1>
+      <table>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>2</td>
+            <td>3</td>
+          </tr>
+          <tr>
+            <td>4</td>
+            <td>5</td>
+            <td>6</td>
+          </tr>
+          <tr>
+            <td>7</td>
+            <td>8</td>
+            <td>9</td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  )
+}
+
+export default Tictactoe
